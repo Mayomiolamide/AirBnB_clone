0c678203faef0b51af1de32dc4065e5df233a788
Mayomiolamide <akintayomayomiolamide@gmail.com>
2023-02-20 19:53:07 -0800

    html

---
+++ models/review.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+""" review module"""
+
+
+
+from models.base_model import BaseModel
+
+
+
+
+
+class Review(BaseModel):
+
+    """ Review model"""
+
+    place_id = ""
+
+    user_id = ""
+
+    text = ""
